154f7c88f6a7b37cc32fb914a230693fd7fc625e
reed <>
2018-10-24 20:09:30 +0800

    Add back LeanCanary and remove java 1.8 dependency

--- app/src/main/java/xyz/aprildown/ultimatemusicpicker/app/App.kt
+++ app/src/main/java/xyz/aprildown/ultimatemusicpicker/app/App.kt
@@ -1,6 +1,7 @@
 package xyz.aprildown.ultimatemusicpicker.app
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 
 /**
  * Created on 2018/9/16.
@@ -9,12 +10,12 @@ import android.app.Application
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-//        if (LeakCanary.isInAnalyzerProcess(this)) {
-//            // This process is dedicated to LeakCanary for heap analysis.
-//            // You should not init your app in this process.
-//            return
-//        }
-//        LeakCanary.install(this)
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
         // Normal app init code...
 
     }
